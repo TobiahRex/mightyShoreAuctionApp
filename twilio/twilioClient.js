a811c65e2be23c18fd369fdde89426cd00b6803b
RexCorpDev <rexcorpfx@gmail.com>
2016-07-14 11:45:12 -0700

    changed to router.post for twilio

--- twilio/twilioClient.js
+++ twilio/twilioClient.js
@@ -5,7 +5,7 @@ const client = require('twilio')(config.accountSid, config.authToken);
 client.sendMessage({
   from: config.sendingNumber,
   to: '+14153210002',
-  body: 'hi Toby.',
+  url: 'https://mighty-shore-12223.herokuapp.com/api/twiml/',
 }, (err, res) => {
   console.log('err: ', err, '\nres: ', res);
 });
